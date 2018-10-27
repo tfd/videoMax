c8ee2c0d4f48c3b17af7d7243a30877058371110
arreche <arreche@gmail.com>
2018-10-27 10:01:11 +0100

    add default route for listing projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
 
 export const APP_ROUTES: Routes = [
-  {path: '', redirectTo: 'editor/1', pathMatch: 'full'},
+  {path: '', loadChildren: './project-list/project-list.module#ProjectListModule'},
   {path: 'search', loadChildren: './youtube-search/youtube-search.module#YoutubeSearchModule'},
   {path: 'editor/:id', loadChildren: './editor/editor.module#EditorModule'},
   // Wildcard route serves as fallback route and has to be
